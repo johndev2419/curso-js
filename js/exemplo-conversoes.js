03657151f88bec7a173b4dd1deb0efc01b396f17
johndev2419 <johnalbert.dev19@gmail.com>
2025-08-04 21:59:30 -0300

    realizados exemplos

---
+++ js/exemplo-conversoes.js
@@ -0,0 +1,13 @@
+//string 
+function converterStringparaInt(){
+    const anoFabricacaoEntradaUsuario = "2024"; //string
+    const anoFabricacao = parseInt (anoFabricacaoEntradaUsuario);
+    alert("ano Fabricação" + anoFabricacao);
+}
+function exemploConverterStringParaFloat(){
+    const precoDocarro = "299800.10" ;
+    const preco = parseFloat(precoDocarro);
+    alert("Preço:" + preco);
+}
+
+exemploConverterStringParaFloat();
